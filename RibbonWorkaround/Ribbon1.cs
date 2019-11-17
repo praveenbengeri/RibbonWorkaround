3244c0fac624bb01e920d78985c01421cf77e677
praveenbengeri <praveenbengeri@hotmail.com>
2019-11-18 05:18:51 +0530

    InitialVersion
    Initial version

---
+++ RibbonWorkaround/Ribbon1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Office = Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace RibbonWorkaround
+{
+    [ComVisible(true)]
+    public class Ribbon1 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon1()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("RibbonWorkaround.Ribbon1.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+            Globals.ThisAddIn.ribbon = this.ribbon; // ansteg note - we added this line so that we can get access to the IRibbonUI object from outside this class.
+        }
+
+        public string GetWorkbookName(Office.IRibbonControl control)
+        {
+            // This callback is hooked up to the "Workbook name" textbox on the custom ribbon.
+            // Excel will take care of firing it at the right time, and you can use the "control.Context"
+            // object to get all the necessary information about the current workbook.
+            Excel.Window window = control.Context;
+            if (window != null)
+            {
+                Excel.Worksheet sheet = window.ActiveSheet;
+                Excel.Workbook book = sheet.Parent;
+                return book.Name;
+            } else
+            {
+                // When the application is booted for the first time this callback will be invoked, but control.Context will be null.
+                // In this case, make the control's value blank (it shouldn't matter, because the user can't see the home tab at this point).
+                return "";
+            }
+            
+        }
+
+        public bool GetIsConfidential(Office.IRibbonControl control)
+        {
+            Excel.Window window = control.Context;
+            if (window != null)
+            {
+                Excel.Worksheet sheet = window.ActiveSheet;
+                String strVal = (sheet.Cells[1, 1].Value2 ?? "").ToString();
+                return strVal.Equals("1");
+            }
+            else
+            {
+                // When the application is booted for the first time this callback will be invoked, but control.Context will be null.
+                // In this case, make the control's value blank (it shouldn't matter, because the user can't see the home tab at this point).
+                return false;
+            }
+            
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ RibbonWorkaround/ThisAddIn.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace RibbonWorkaround
+{
+    public partial class ThisAddIn
+    {
+
+        public Office.IRibbonUI ribbon; // ansteg note - this is populated in the Ribbon_Load method of Ribbon1. It gives access to the ribbon from other parts of our application (see Application_SheetChange below).
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.WorkbookOpen += Application_WorkbookOpen;
+            ((Excel.AppEvents_Event)this.Application).NewWorkbook += ThisAddIn_NewWorkbook;
+            this.Application.SheetChange += Application_SheetChange;
+        }
+
+        private void ThisAddIn_NewWorkbook(Excel.Workbook Wb)
+        {
+            ribbon.Invalidate();
+        }
+
+        private void Application_WorkbookOpen(Excel.Workbook Wb)
+        {
+            ribbon.Invalidate();
+        }
+
+        private void Application_SheetChange(object Sh, Excel.Range Target)
+        {
+            ribbon.Invalidate();
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon1();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
